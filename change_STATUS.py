f1fc951ed0d5cb51a44744829749dc50a8c652fb
JolyDog <tvaliaschvili1234@gmail.com>
2017-06-19 18:21:14 +0300

    Add Captcha file

--- change_STATUS.py
+++ change_STATUS.py
@@ -3,13 +3,6 @@ from datetime import datetime, time, date
 import time
 import vk_api
 
-key = ""
-vk = vk_api.VkApi(login = '79888726851', password = 'Andrew1975', captcha_handler=captcha_handler)
-vk2 = vk_api.VkApi(login = 'tvaliaschvili1234@gmail.com', password = '89888726851i', captcha_handler=captcha_handler)
-vk.auth()
-vk2.auth()
-a = 1
-
 def change_status(text):	
 	status = {'text': text}
 	vk.method('status.set', status)
@@ -29,10 +22,21 @@ def captcha_handler(captcha):
 
     return captcha.try_again(key)
 
+key = ""
+vk = vk_api.VkApi(login = '79888726851', password = 'Andrew1975', captcha_handler=captcha_handler)
+vk2 = vk_api.VkApi(login = 'tvaliaschvili1234@gmail.com', password = '89888726851i', captcha_handler=captcha_handler)
+vk.auth()
+vk2.auth()
+a = 1
+k = 0
+
+
+
 
 
 
 while  a == 1:
+	k = k + 1
 	for x in xrange(1,10):
 		d = datetime.today()	
 		t = str(d.time())
