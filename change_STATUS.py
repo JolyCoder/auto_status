59f8be765e85b9ea96fe0a0f84d3bf7c4171f003
JolyDog <tvaliaschvili1234@gmail.com>
2017-07-14 23:41:47 +0300

    Add ASCII support, and bug fixed

--- change_STATUS.py
+++ change_STATUS.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python 
+# -*- coding: utf-8 -*- 
+
 import urllib
 from datetime import datetime, time, date
 import time
@@ -8,19 +11,24 @@ a = 1
 # Переменные #
 
 # Функции #
-def auth(vkLogin1, vkPass1, captchaHandler1, vkLogin2, vkPass2, captchaHandler2, vkLogin3, vkPass3, captchaHandler3,count):
+def auth(count, vkLogin1 = '', vkPass1 = '', captchaHandler1 = '', vkLogin2 = '', vkPass2 = '', captchaHandler2 = '', vkLogin3 = '', vkPass3 = '', captchaHandler3 = ''):
 	key = ""
-	vk = vk_api.VkApi(login = vkLogin, password = vkPass, captcha_handler=captchaHandler)
-	vk.auth()
+	vk = vk_api.VkApi(login = vkLogin1, password = vkPass1, captcha_handler=captchaHandler1)
 	if count == 1:
 		vk = vk_api.VkApi(login = vkLogin1, password = vkPass1, captcha_handler=captchaHandler1)
+		vk.auth()
 	elif count == 2:
 		vk = vk_api.VkApi(login = vkLogin1, password = vkPass1, captcha_handler=captchaHandler1)
+		vk.auth()
 		vk2 = vk_api.VkApi(login = vkLogin2, password = vkPass2, captcha_handler=captchaHandler2)
+		vk2.auth()
 	elif count == 3:
 		vk = vk_api.VkApi(login = vkLogin1, password = vkPass1, captcha_handler=captchaHandler1)
-		vk = vk_api.VkApi(login = vkLogin2, password = vkPass2, captcha_handler=captchaHandler2)
-		vk = vk_api.VkApi(login = vkLogin3, password = vkPass3, captcha_handler=captchaHandler3)
+		vk.auth()
+		vk2 = vk_api.VkApi(login = vkLogin2, password = vkPass2, captcha_handler=captchaHandler2)
+		vk2.auth()
+		vk3= vk_api.VkApi(login = vkLogin3, password = vkPass3, captcha_handler=captchaHandler3)
+		vk3.auth()
 	k = 0
 
 def change_status(text):	
@@ -44,7 +52,7 @@ def captcha_handler(captcha):
 # Функции #
 
 # Авторизация #
-auth('login1', 'password1', captcha_handler)
+auth(3, 'login1', 'password1', captcha_handler)
 # Авторизация #
 
 # Основной код, по изменению статуса #
